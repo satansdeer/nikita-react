f1aabdeabbe20bb4714561ca850da6bc41a6a01d
Maksim Ivanov <satansdeer@gmail.com>
2020-09-22 21:59:22 +0200

    Fixed

--- src/TestComponent.js
+++ src/TestComponent.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 
 export const TestComponent = ({ count, onChange }) => {
   const [distance, setDistance] = useState(0);
@@ -6,35 +6,37 @@ export const TestComponent = ({ count, onChange }) => {
   const countRef = useRef(null);
   const isDragged = useRef(null);
 
+  const distanceRef = useRef(null);
+  useEffect(() => {
+    distanceRef.current = distance;
+  },[distance])
+
   countRef.current = count;
 
   const onMouseDown = () => {
-    console.log("!!!");
     isDragged.current = true;
   };
 
+  const onMouseUp = () => {
+    if (!isDragged.current) {
+      return;
+    }
+    isDragged.current = false;
+    onChange(distanceRef.current);
+  }
+
+  const onMouseMove = () => {
+    if (!isDragged.current) {
+      return;
+    }
+
+    setDistance((d) => d + 1);
+  }
+
   useEffect(() => {
-    const onMouseUp = () => {
-      if (!isDragged.current) {
-        return;
-      }
-      isDragged.current = false;
-      onChange(distance);
-    };
-
-    const onMouseMove = () => {
-      if (!isDragged.current) {
-        return;
-      }
-
-      setDistance((d) => d + 1);
-    };
-
-    console.log("Effect init")
     document.addEventListener("mouseup", onMouseUp);
     document.addEventListener("mousemove", onMouseMove);
     return () => {
-      console.log("Effect return")
       document.removeEventListener("mousedown", onMouseUp);
       document.removeEventListener("mousedown", onMouseMove);
     };
@@ -42,10 +44,9 @@ export const TestComponent = ({ count, onChange }) => {
 
   return (
     <>
-    {console.log("rerender")}
-    <p onMouseDown={onMouseDown}>
-      Hello I'm TestComponent {count}: {distance}
-    </p>
+      <p onMouseDown={onMouseDown}>
+        Hello I'm TestComponent {count}: {distance}
+      </p>
     </>
   );
 };
